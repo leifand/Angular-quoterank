2d54428c7f3d4f8fab927975849adf81f96478ef
Leif Anderson <leif.v.anderson@gmail.com>
2017-09-19 17:44:28 -0500

    quote ranks - built from class demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuoteDashboardComponent } from './quote-dashboard/quote-dashboard.component';
+import { QuoteNewComponent } from './quote-dashboard/quote-new/quote-new.component';
+import { QuoteListComponent } from './quote-dashboard/quote-list/quote-list.component';
+import { QuoteSingleComponent } from './quote-dashboard/quote-single/quote-single.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteDashboardComponent,
+    QuoteNewComponent,
+    QuoteListComponent,
+    QuoteSingleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
